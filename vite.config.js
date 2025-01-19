739e0965297d36b535934a3c2ce90c373cff3a44
Javeria Hamid <javeriahamidali@gmail.com>
2025-01-19 19:39:13 +0500

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // Ensure this is present
+
+export default defineConfig({
+  plugins: [react()],
+});
