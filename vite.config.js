6478330a16215ff7456098bf12bd7eb36d51af3b
Javeria Hamid <javeriahamidali@gmail.com>
2025-01-19 19:42:09 +0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // Ensure this is present
-
-export default defineConfig({
-  plugins: [react()],
-});
